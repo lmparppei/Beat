2809af27249b7224300aabf8195be61e1938b1de
lmparppei <lauri-matti@kapitan.fi>
2022-11-09 16:06:05 +0200

    Organize categories and extensions

---
+++ Beat/Overall Categories/BeatAttributedStringExtensions.swift
@@ -0,0 +1,41 @@
+//
+//  BeatAttributedStringExtensions.swift
+//  Beat
+//
+//  Created by Lauri-Matti Parppei on 4.11.2022.
+//  Copyright © 2022 Lauri-Matti Parppei. All rights reserved.
+//
+
+import Foundation
+
+extension NSAttributedString {
+	func height(containerWidth: CGFloat) -> CGFloat {
+		let rect = self.boundingRect(with: CGSize.init(width: containerWidth, height: CGFloat.greatestFiniteMagnitude),
+									 options: [.usesLineFragmentOrigin, .usesFontLeading],
+									 context: nil)
+		return ceil(rect.size.height)
+	}
+	
+	func width(containerHeight: CGFloat) -> CGFloat {
+		let rect = self.boundingRect(with: CGSize.init(width: CGFloat.greatestFiniteMagnitude, height: containerHeight),
+									 options: [.usesLineFragmentOrigin, .usesFontLeading],
+									 context: nil)
+		return ceil(rect.size.width)
+	}
+	/// Simply returns a range of the full string (`0...length`)
+	var range:NSRange {
+		get { return NSRange(location: 0, length: self.length) }
+	}
+	
+	func uppercased() -> NSAttributedString {
+
+		let result = NSMutableAttributedString(attributedString: self)
+
+		result.enumerateAttributes(in: NSRange(location: 0, length: length), options: []) {_, range, _ in
+			result.replaceCharacters(in: range, with: (string as NSString).substring(with: range).uppercased())
+		}
+
+		return result
+	}
+}
+
---
+++ Beat/Overall Categories/NSString+Levenshtein.m
@@ -0,0 +1,98 @@
+//
+//  NSString+Levenshtein.m
+//
+
+#import "NSString+Levenshtein.h"
+
+@implementation NSString (Levenshtein)
+
+// default match: 0
+// default cost: 1
+
+-(float)compareWithString:(NSString *)comparisonString
+{
+	NSString *originalString = self;
+	
+	// Normalize strings
+	[originalString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+	[comparisonString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+
+	originalString = [originalString lowercaseString];
+	comparisonString = [comparisonString lowercaseString];
+
+	// Step 1 (Steps follow description at http://www.merriampark.com/ld.htm)
+	NSInteger k, i, j, cost, * d, distance;
+
+	NSInteger n = [originalString length];
+	NSInteger m = [comparisonString length];
+
+	if( n++ != 0 && m++ != 0 ) {
+
+		d = malloc( sizeof(NSInteger) * m * n );
+
+		// Step 2
+		for( k = 0; k < n; k++)
+			d[k] = k;
+
+		for( k = 0; k < m; k++)
+			d[ k * n ] = k;
+
+		// Step 3 and 4
+		for( i = 1; i < n; i++ )
+			for( j = 1; j < m; j++ ) {
+
+				// Step 5
+				if( [originalString characterAtIndex: i-1] ==
+				   [comparisonString characterAtIndex: j-1] )
+					cost = 0;
+				else
+					cost = 1;
+
+				// Step 6
+				d[ j * n + i ] = [self smallestOf: d [ (j - 1) * n + i ] + 1
+				andOf: d[ j * n + i - 1 ] +  1
+				andOf: d[ (j - 1) * n + i - 1 ] + cost ];
+
+				// This conditional adds Damerau transposition to Levenshtein distance
+				if( i>1 && j>1 && [originalString characterAtIndex: i-1] ==
+					[comparisonString characterAtIndex: j-2] &&
+					[originalString characterAtIndex: i-2] ==
+					[comparisonString characterAtIndex: j-1] )
+				{
+					d[ j * n + i] = [self smallestOf: d[ j * n + i ]
+					andOf: d[ (j - 2) * n + i - 2 ] + cost ];
+				}
+			}
+
+		distance = d[ n * m - 1 ];
+
+		free( d );
+
+		return distance;
+	}
+	return 0.0;
+}
+
+// Return the minimum of a, b and c - used by compareString:withString:
+-(NSInteger)smallestOf:(NSInteger)a andOf:(NSInteger)b andOf:(NSInteger)c
+{
+	NSInteger min = a;
+	if ( b < min )
+		min = b;
+
+	if( c < min )
+		min = c;
+
+	return min;
+}
+
+-(NSInteger)smallestOf:(NSInteger)a andOf:(NSInteger)b
+{
+	NSInteger min=a;
+	if (b < min)
+		min=b;
+
+	return min;
+}
+
+@end
