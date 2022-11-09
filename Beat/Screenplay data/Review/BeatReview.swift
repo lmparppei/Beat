0929dee6b25c506dd7c56030f074f86b2bfd0137
lmparppei <lauri-matti@kapitan.fi>
2022-11-09 16:12:49 +0200

    Extension moved elsewhere

--- Beat/Screenplay data/Review/BeatReview.swift
+++ Beat/Screenplay data/Review/BeatReview.swift
@@ -362,22 +362,6 @@ class BeatReviewEditor: NSViewController, NSTextViewDelegate, BeatReviewDelegate
 	}
 }
 
-extension NSAttributedString {
-	func height(containerWidth: CGFloat) -> CGFloat {
-		let rect = self.boundingRect(with: CGSize.init(width: containerWidth, height: CGFloat.greatestFiniteMagnitude),
-									 options: [.usesLineFragmentOrigin, .usesFontLeading],
-									 context: nil)
-		return ceil(rect.size.height)
-	}
-
-	func width(containerHeight: CGFloat) -> CGFloat {
-		let rect = self.boundingRect(with: CGSize.init(width: CGFloat.greatestFiniteMagnitude, height: containerHeight),
-									 options: [.usesLineFragmentOrigin, .usesFontLeading],
-									 context: nil)
-		return ceil(rect.size.width)
-	}
-}
-
 class BeatReviewTextView:NSTextView {
 	weak var reviewDelegate: BeatReviewDelegate?
 	
