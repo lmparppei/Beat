56900a6c32b808838ed70bc9b910ecffb7118599
lmparppei <lauri-matti@kapitan.fi>
2023-01-11 15:08:30 +0200

    Split user defaults to a framework

---
+++ BeatSharedUserDefaults/BeatSharedUserDefaults.h
@@ -0,0 +1,18 @@
+//
+//  BeatSharedUserDefaults.h
+//  BeatSharedUserDefaults
+//
+//  Created by Lauri-Matti Parppei on 11.1.2023.
+//
+
+#import <Foundation/Foundation.h>
+#import "BeatUserDefaults.h"
+
+//! Project version number for BeatSharedUserDefaults.
+FOUNDATION_EXPORT double BeatSharedUserDefaultsVersionNumber;
+
+//! Project version string for BeatSharedUserDefaults.
+FOUNDATION_EXPORT const unsigned char BeatSharedUserDefaultsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BeatSharedUserDefaults/PublicHeader.h>
+
