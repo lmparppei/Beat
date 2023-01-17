231b6326b28d8e6f50f7370a923a5980c12d3411
lmparppei <lauri-matti@kapitan.fi>
2023-01-17 23:59:10 +0200

    Move user defaults to a framework

---
+++ Frameworks/BeatDefaults/BeatDefaults/BeatDefaults.h
@@ -0,0 +1,16 @@
+//
+//  BeatDefaults.h
+//  BeatDefaults
+//
+//  Created by Lauri-Matti Parppei on 17.1.2023.
+//
+
+#import <Foundation/Foundation.h>
+#import "BeatUserDefaults.h"
+
+//! Project version number for BeatDefaults.
+FOUNDATION_EXPORT double BeatDefaultsVersionNumber;
+
+//! Project version string for BeatDefaults.
+FOUNDATION_EXPORT const unsigned char BeatDefaultsVersionString[];
+
