64615e9f74ce44a6c0348125bdb2b3b464e4543d
lmparppei <lauri-matti@kapitan.fi>
2023-01-26 00:28:46 +0200

    Added character index shorthands

--- Beat/Overall Categories/BeatStringExtensions.swift
+++ Beat/Overall Categories/BeatStringExtensions.swift
@@ -65,3 +65,18 @@ extension String {
 		return updatedString
 	}
 }
+
+extension StringProtocol {
+	subscript(offset: Int) -> Character { self[index(startIndex, offsetBy: offset)] }
+	subscript(range: Range<Int>) -> SubSequence {
+		let startIndex = index(self.startIndex, offsetBy: range.lowerBound)
+		return self[startIndex..<index(startIndex, offsetBy: range.count)]
+	}
+	subscript(range: ClosedRange<Int>) -> SubSequence {
+		let startIndex = index(self.startIndex, offsetBy: range.lowerBound)
+		return self[startIndex..<index(startIndex, offsetBy: range.count)]
+	}
+	subscript(range: PartialRangeFrom<Int>) -> SubSequence { self[index(startIndex, offsetBy: range.lowerBound)...] }
+	subscript(range: PartialRangeThrough<Int>) -> SubSequence { self[...index(startIndex, offsetBy: range.upperBound)] }
+	subscript(range: PartialRangeUpTo<Int>) -> SubSequence { self[..<index(startIndex, offsetBy: range.upperBound)] }
+}
