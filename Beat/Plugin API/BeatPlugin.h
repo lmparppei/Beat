f5259f1c4b08ee1873b306fc01266c5e0881a10d
lmparppei <lauri-matti@kapitan.fi>
2023-07-20 12:14:49 +0300

    BeatCore integrity

--- Beat/Plugin API/BeatPlugin.h
+++ Beat/Plugin API/BeatPlugin.h
@@ -56,18 +56,97 @@
 @class BeatPluginControlMenuItem;
 @class BeatPaginationManager;
 @class BeatPagination;
+@class OutlineScene;
+
+#pragma mark - JS API exports
 
 @protocol BeatPluginExports <JSExport>
-@property (readonly) Line* currentLine;
-@property (readonly) OutlineScene* currentScene;
+
+#pragma mark Plugin control
+/// Stop plugin execution
+- (void)end;
+/// Restart current plugin
+- (void)restart;
+/// Crash the app
+- (void)crash;
+
+
+#pragma mark System access
+/// Check compatibility with Beat version. Basically used for checking if Beat version is out of date.
+- (bool)compatibleWith:(NSString*)version;
+/// Returns `true` when you can use promises in JS
+- (bool)promisesAvailable;
+/// Sets a property value in host document. Only for those who REALLY, REALLY, __REALLY__ KNOW WHAT THEY ARE DOING
+JSExportAs(setPropertyValue, - (void)setPropertyValue:(NSString*)key value:(id)value);
+/// Executes a run-time ObjC call. This is for the people who, really, and let me emphasize, __actually__ know what the fuck they are doing. No plugins should ever use this method. Purely for testing and hacking purposes.
+JSExportAs(objc_call, - (id)objc_call:(NSString*)methodName args:(NSArray*)arguments);
+
+
+#pragma mark Parsed content
+/// List of Beat line types
+@property (nonatomic,readonly) NSDictionary *type;
+/// Returns the actual parser object in the document
 @property (weak, readonly) ContinuousFountainParser *currentParser;
 
+/// Create a new parser with given raw string (__NOTE__: Doesn't support document settings, revisions, etc.)
+- (ContinuousFountainParser*)parser:(NSString*)string;
+
+/// Returns all parsed lines
+- (NSArray*)lines;
+/// Returns the current outline
+- (NSArray*)outline;
+/// Returns the current outline excluding any structural elements (namely `sections`)
+- (NSArray*)scenes;
+
+/// Returns the full outline as a JSON string
+- (NSString*)outlineAsJSON;
+/// Returns all scenes as a JSON string
+- (NSString*)scenesAsJSON;
+/// Returns all lines as a JSON string
+- (NSString*)linesAsJSON;
+/// Returns the line at given position in document
+- (Line*)lineAtPosition:(NSInteger)index;
+/// Returns lines in given scene.
+- (NSArray*)linesForScene:(OutlineScene*)scene;
+/// Creates the outline from scratch
+- (void)createOutline;
+/// Returns the full raw text (excluding settings block)
+- (NSString*)getText;
+
+
+#pragma mark Contextual line and scene info
+/// Currently edited line
+@property (readonly) Line* currentLine;
+/// Currently edited scene
+@property (readonly) OutlineScene* currentScene;
+
+
+#pragma mark Document settings
+/// Returns a document setting prefixed by current plugin name
+- (id)getDocumentSetting:(NSString*)key;
+/// Returns a non-prefixed document setting
+- (id)getRawDocumentSetting:(NSString*)key;
+/// For those who REALLY know what they're doing
+- (id)getPropertyValue:(NSString*)key;
+/// Sets a document setting without plugin prefix. Can be used to tweak actual document data.
+JSExportAs(setRawDocumentSetting, - (void)setRawDocumentSetting:(NSString*)settingName setting:(id)value);
+/// Sets a document setting, prefixed by plugin name, so you won't mess up settings for other plugins.
+JSExportAs(setDocumentSetting, - (void)setDocumentSetting:(NSString*)settingName setting:(id)value);
+
+
+#pragma mark User settings
+/// Sets a user default (`key`, `value`)
+JSExportAs(setUserDefault, - (void)setUserDefault:(NSString*)settingName setting:(id)value);
+/// Returns a user default (`key`)
+JSExportAs(getUserDefault, - (id)getUserDefault:(NSString*)settingName);
+
+
+#pragma mark Listeners
 @property (nonatomic) bool onPreviewFinishedDisabled;
 @property (nonatomic) bool onOutlineChangeDisabled;
 @property (nonatomic) bool onSelectionChangeDisabled;
 @property (nonatomic) bool onTextChangeDisabled;
 @property (nonatomic) bool onSceneIndexUpdateDisabled;
-@property (nonatomic,readonly) NSDictionary *type;
 
 /// Forces the plugin to stay in memory
 - (void)makeResident;
@@ -75,10 +154,13 @@
 // Alias + actual methods for update methods
 - (void)setUpdate:(JSValue*)updateMethod;
 - (void)onTextChange:(JSValue*)updateMethod;
+
 - (void)setSelectionUpdate:(JSValue *)updateMethod;
 - (void)onSelectionChange:(JSValue*)updateMethod;
+
 - (void)onOutlineChange:(JSValue*)updateMethod;
 - (void)onSceneIndexUpdate:(JSValue*)updateMethod;
+
 - (void)onDocumentBecameMain:(JSValue*)updateMethod;
 - (void)onSceneHeadingAutocompletion:(JSValue*)callback;
 - (void)onCharacterAutocompletion:(JSValue*)callback;
@@ -87,31 +169,60 @@
 - (void)onDocumentSaved:(JSValue*)updateMethod;
 - (void)onEscape:(JSValue*)updateMethod;
 
+
+#pragma mark General editor and app access
+/// Creates a new document with given string
+/// - note: The string can contain a settings block
+- (void)newDocument:(NSString*)string;
+/// Creates a new `Document` object without actually opening the window
+- (id)newDocumentObject:(NSString*)string;
+
+/// Current screen dimensions
+- (NSArray*)screen;
+
+	#if !TARGET_OS_IOS
+	/// Returns a print info object (macOS only)
+	- (NSPrintInfo*)printInfo;
+	/// Window dimensions
+	- (NSArray*)windowFrame;
+	/// Alias for windowFrame
+	- (NSArray*)getWindowFrame;
+	/// Sets the window frame
+	JSExportAs(setWindowFrame, - (void)setWindowFrameX:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height);
+	#endif
+
+
+#pragma mark Logging
+/// Logs given string in developer console
 - (void)log:(NSString*)string;
+/// Opens the console programmatically
 - (void)openConsole;
 
+
+#pragma mark Editor view
+/// Focuses the editor text view
+- (void)focusEditor;
+
+/// Scrolls to given position in document
 - (void)scrollTo:(NSInteger)location;
+/// Scrolls to given line index
 - (void)scrollToLineIndex:(NSInteger)index;
+/// Scrolls to given line
 - (void)scrollToLine:(Line*)line;
+/// Scrolls to given scene heading
 - (void)scrollToScene:(OutlineScene*)scene;
+/// Scrolls to the scene heading at given outline index
 - (void)scrollToSceneIndex:(NSInteger)index;
-
-- (void)newDocument:(NSString*)string;
-- (id)newDocumentObject:(NSString*)string;
-- (NSString*)getText;
-
-- (NSPrintInfo*)printInfo;
-
-- (NSArray*)lines;
-- (NSArray*)outline;
-- (NSArray*)scenes;
-- (NSString*)scenesAsJSON;
-- (NSString*)outlineAsJSON;
-- (NSString*)linesAsJSON;
-- (Line*)lineAtPosition:(NSInteger)index;
-
+/// Returns the selected range in editor
 - (NSRange)selectedRange;
-- (NSArray*)linesForScene:(id)scene;
+
+/// Move to next tab in document window
+- (void)nextTab;
+/// Move to previoustab in document window
+- (void)previousTab;
+
+
+#pragma mark File I/O
 /// Read any (text) file into string variable
 - (NSString*)fileToString:(NSString*)path;
 /// Read a PDF file into string variable
@@ -121,23 +232,27 @@
 - (NSString*)assetAsString:(NSString*)filename;
 /// Asset from inside the app container
 - (NSString*)appAssetAsString:(NSString*)filename;
-/// Stop plugin execution
-- (void)end;
+/// Write a string to file in given path. You can't access files unless they are in the container or user explicitly selected them using a save dialog.
+JSExportAs(writeToFile, - (bool)writeToFile:(NSString*)path content:(NSString*)content);
 
+/// Displays an open dialog
+JSExportAs(openFile, - (void)openFile:(NSArray*)formats callBack:(JSValue*)callback);
+/// Displays an open dialog with the option to select multiple files
+JSExportAs(openFiles, - (void)openFiles:(NSArray*)formats callBack:(JSValue*)callback);
+/// Displays a save dialog
+JSExportAs(saveFile, - (void)saveFile:(NSString*)format callback:(JSValue*)callback);
+
+#pragma mark Tagging
 /// Returns all tags in the scene
 - (NSDictionary*)tagsForScene:(OutlineScene*)scene;
 /// Returns all available tag names
 - (NSArray*)availableTags;
 
-/// Current screen dimensions
-- (NSArray*)screen;
-/// Window dimensions
-- (NSArray*)windowFrame;
-/// Alias for windowFrame
-- (NSArray*)getWindowFrame;
 
-
+#pragma mark Multi-threading
+/// Dispatch a block into a background thread
 - (void)async:(JSValue*)callback;
+/// Dispatch a block into the main thread
 - (void)sync:(JSValue*)callback;
 /// Alias for async
 - (void)dispatch:(JSValue*)callback;
@@ -146,55 +261,40 @@
 /// Returns `true` if the current operation happens in main thread
 - (bool)isMainThread;
 
-/// Focuses the editor text view
-- (void)focusEditor;
 
-/// Returns a document setting prefixed by current plugin name
-- (id)getDocumentSetting:(NSString*)key;
-/// Returns a non-prefixed document setting
-- (id)getRawDocumentSetting:(NSString*)key;
-/// For those who REALLY know what they're doing
-- (id)getPropertyValue:(NSString*)key;
-
-/// Returns the current parser in document
-- (ContinuousFountainParser*)parser:(NSString*)string;
-/// Creates the outline from scratch
-- (void)createOutline;
-
-/// Returns old-style paginator object
-- (BeatPaginationManager*)paginator:(NSArray*)lines;
-/// Returns a NEW pagination object
-- (BeatPaginationManager*)pagination;
-/// Returns the CURRENT pagination in document
+#pragma mark Pagination
+/// Returns the CURRENT pagination manager in document
 - (BeatPaginationManager*)currentPagination;
+/// Creates and returns a new pagination manager with given lines as input
+- (BeatPaginationManager*)paginator:(NSArray*)lines;
+/// Creates and returns a NEW pagination manager
+- (BeatPaginationManager*)pagination;
 
-/// Reformat given line
+
+#pragma mark Reformatting
+JSExportAs(reformatRange, - (void)reformatRange:(NSInteger)loc len:(NSInteger)len);
 - (void)reformat:(Line*)line;
 
-/// Check compatibility with Beat version. Basically used for checking if Beat version is out of date.
-- (bool)compatibleWith:(NSString*)version;
 
+#pragma mark Widgets
 /// Add widget into sidebar
 - (BeatPluginUIView*)widget:(CGFloat)height;
+JSExportAs(button, - (BeatPluginUIButton*)button:(NSString*)name action:(JSValue*)action frame:(NSRect)frame);
+JSExportAs(dropdown, - (BeatPluginUIDropdown*)dropdown:(NSArray<NSString *> *)items action:(JSValue*)action frame:(NSRect)frame);
+JSExportAs(checkbox, - (BeatPluginUICheckbox*)checkbox:(NSString*)title action:(JSValue*)action frame:(NSRect)frame);
+JSExportAs(label, - (BeatPluginUILabel*)label:(NSString*)title frame:(NSRect)frame color:(NSString*)color size:(CGFloat)size font:(NSString*)fontName);
 
-/// Move to next tab in document window
-- (void)nextTab;
-/// Move to previoustab in document window
-- (void)previousTab;
 
+#pragma mark Speak synthesizer
 /// Create new speech synthesis instance
 - (BeatSpeak*)speakSynth;
 
-/// Restart current plugin
-- (void)restart;
 
-/// Crash the app
-- (void)crash;
+#pragma mark Timer
+JSExportAs(timer, - (BeatPluginTimer*)timerFor:(CGFloat)seconds callback:(JSValue*)callback repeats:(bool)repeats);
 
-/// Returns `true` when you can use promises in JS
-- (bool)promisesAvailable;
 
-// Revisions
+#pragma mark Revisions
 /// Returns all the revised ranges in attributed text
 - (NSDictionary*)revisedRanges;
 /// Bakes current revisions into lines
@@ -202,53 +302,45 @@
 /// Bakes revisions in given range
 JSExportAs(bakeRevisionsInRange, - (void)bakeRevisionsInRange:(NSInteger)loc len:(NSInteger)len);
 
-/// Sets a property value in host document. Only for those who REALLY, REALLY, __REALLY__ KNOW WHAT THEY ARE DOING
-JSExportAs(setPropertyValue, - (void)setPropertyValue:(NSString*)key value:(id)value);
+
+#pragma mark Text I/O
 
 JSExportAs(setSelectedRange, - (void)setSelectedRange:(NSInteger)start to:(NSInteger)length);
 JSExportAs(addString, - (void)addString:(NSString*)string toIndex:(NSUInteger)index);
 JSExportAs(replaceRange, - (void)replaceRange:(NSInteger)from length:(NSInteger)length withString:(NSString*)string);
+
+
+#pragma mark Modal windows
+/// Displays a simple modal alert box
 JSExportAs(alert, - (void)alert:(NSString*)title withText:(NSString*)info);
+/// Displays a text input prompt.
+/// - returns String value. Value is `nil` if the user pressed cancel.
 JSExportAs(prompt, - (NSString*)prompt:(NSString*)prompt withInfo:(NSString*)info placeholder:(NSString*)placeholder defaultText:(NSString*)defaultText);
+/// Displays a confirmation modal. Returns `true` if the user pressed OK.
 JSExportAs(confirm, - (bool)confirm:(NSString*)title withInfo:(NSString*)info);
+/// Displays a dropdown prompt with a list of strings. Returns the selected string. Return value is `nil` if the user pressed cancel.
 JSExportAs(dropdownPrompt, - (NSString*)dropdownPrompt:(NSString*)prompt withInfo:(NSString*)info items:(NSArray*)items);
-JSExportAs(setUserDefault, - (void)setUserDefault:(NSString*)settingName setting:(id)value);
-JSExportAs(getUserDefault, - (id)getUserDefault:(NSString*)settingName);
-JSExportAs(setRawDocumentSetting, - (void)setRawDocumentSetting:(NSString*)settingName setting:(id)value);
-JSExportAs(setDocumentSetting, - (void)setDocumentSetting:(NSString*)settingName setting:(id)value);
-JSExportAs(openFile, - (void)openFile:(NSArray*)formats callBack:(JSValue*)callback);
-JSExportAs(openFiles, - (void)openFiles:(NSArray*)formats callBack:(JSValue*)callback);
-JSExportAs(saveFile, - (void)saveFile:(NSString*)format callback:(JSValue*)callback);
-JSExportAs(writeToFile, - (bool)writeToFile:(NSString*)path content:(NSString*)content);
+/// Displays a modal with given settings. Consult the wiki for correct dictionary keys and values.
+JSExportAs(modal, -(NSDictionary*)modal:(NSDictionary*)settings callback:(JSValue*)callback);
+
+#pragma mark Displaying HTML content
 JSExportAs(htmlPanel, - (void)htmlPanel:(NSString*)html width:(CGFloat)width height:(CGFloat)height callback:(JSValue*)callback cancelButton:(bool)cancelButton);
 JSExportAs(htmlWindow, - (NSPanel*)htmlWindow:(NSString*)html width:(CGFloat)width height:(CGFloat)height callback:(JSValue*)callback);
-JSExportAs(timer, - (BeatPluginTimer*)timerFor:(CGFloat)seconds callback:(JSValue*)callback repeats:(bool)repeats);
 JSExportAs(setColorForScene, -(void)setColor:(NSString *)color forScene:(id)scene);
-JSExportAs(modal, -(NSDictionary*)modal:(NSDictionary*)settings callback:(JSValue*)callback);
 JSExportAs(printHTML, - (void)printHTML:(NSString*)html settings:(NSDictionary*)settings callback:(JSValue*)callback);
 
-// Window interactions
-JSExportAs(setWindowFrame, - (void)setWindowFrameX:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height);
 
-// Text highlights
+#pragma mark Text highlighting
 JSExportAs(textHighlight, - (void)textHighlight:(NSString*)hexColor loc:(NSInteger)loc len:(NSInteger)len);
 JSExportAs(textBackgroundHighlight, - (void)textBackgroundHighlight:(NSString*)hexColor loc:(NSInteger)loc len:(NSInteger)len);
 JSExportAs(removeTextHighlight, - (void)removeTextHighlight:(NSInteger)loc len:(NSInteger)len);
 JSExportAs(removeBackgroundHighlight, - (void)removeBackgroundHighlight:(NSInteger)loc len:(NSInteger)len);
-JSExportAs(reformatRange, - (void)reformatRange:(NSInteger)loc len:(NSInteger)len);
 
-// Widget UI
-JSExportAs(button, - (BeatPluginUIButton*)button:(NSString*)name action:(JSValue*)action frame:(NSRect)frame);
-JSExportAs(dropdown, - (BeatPluginUIDropdown*)dropdown:(NSArray<NSString *> *)items action:(JSValue*)action frame:(NSRect)frame);
-JSExportAs(checkbox, - (BeatPluginUICheckbox*)checkbox:(NSString*)title action:(JSValue*)action frame:(NSRect)frame);
-JSExportAs(label, - (BeatPluginUILabel*)label:(NSString*)title frame:(NSRect)frame color:(NSString*)color size:(CGFloat)size font:(NSString*)fontName);
 
-// Import/Export
+#pragma mark Import / export plugin handlers
 JSExportAs(importHandler, - (void)importHandler:(NSArray*)extensions callback:(JSValue*)callback);
 JSExportAs(exportHandler, - (void)exportHandler:(NSArray*)extensions callback:(JSValue*)callback);
 
-// Call objective C methods directly
-JSExportAs(objc_call, - (id)objc_call:(NSString*)methodName args:(NSArray*)arguments);
 
 // Create a new line element
 JSExportAs(line, - (Line*)lineWithString:(NSString*)string type:(LineType)type);
@@ -262,7 +354,9 @@ JSExportAs(menuItem, - (BeatPluginControlMenuItem*)menuItem:(NSString*)title sho
 JSExportAs(submenu, - (NSMenuItem*)submenu:(NSString*)name items:(NSArray<BeatPluginControlMenuItem*>*)items);
 @end
 
-// Interfacing with the document
+
+#pragma mark - Host document delegate
+
 @protocol BeatPluginDelegate <NSObject>
 
 // macOS-only
