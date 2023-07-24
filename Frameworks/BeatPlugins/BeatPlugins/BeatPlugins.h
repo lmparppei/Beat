15eae99fbbea78f49d139bc465ed77e60f4c5444
lmparppei <lauri-matti@kapitan.fi>
2023-07-24 17:01:52 +0300

    Plugin API framework

---
+++ Frameworks/BeatPlugins/BeatPlugins/BeatPlugins.h
@@ -0,0 +1,18 @@
+//
+//  BeatPlugins.h
+//  BeatPlugins
+//
+//  Created by Lauri-Matti Parppei on 24.7.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BeatPlugins.
+FOUNDATION_EXPORT double BeatPluginsVersionNumber;
+
+//! Project version string for BeatPlugins.
+FOUNDATION_EXPORT const unsigned char BeatPluginsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BeatPlugins/PublicHeader.h>
+
+
