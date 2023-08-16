d13b99523fa2d65b593bf6ad93275f433b2eb037
lmparppei <lauri-matti@kapitan.fi>
2023-08-16 14:05:07 +0300

    Theme editor fix

--- Beat/User Interface/Theme Editor/ThemeEditor.m
+++ Beat/User Interface/Theme Editor/ThemeEditor.m
@@ -12,7 +12,54 @@
 #import "Beat-Swift.h"
 
 @interface ThemeEditor ()
+@property (nonatomic, weak) IBOutlet NSColorWell *backgroundLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *backgroundDark;
+@property (nonatomic, weak) IBOutlet NSColorWell *textLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *textDark;
+@property (nonatomic, weak) IBOutlet NSColorWell *marginLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *marginDark;
+@property (nonatomic, weak) IBOutlet NSColorWell *selectionLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *selectionDark;
+@property (nonatomic, weak) IBOutlet NSColorWell *invisibleTextLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *invisibleTextDark;
+@property (nonatomic, weak) IBOutlet NSColorWell *pageNumberLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *pageNumberDark;
+@property (nonatomic, weak) IBOutlet NSColorWell *commentLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *commentDark;
+@property (nonatomic, weak) IBOutlet NSColorWell *caretLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *caretDark;
+@property (nonatomic, weak) IBOutlet NSColorWell *synopsisLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *synopsisDark;
+@property (nonatomic, weak) IBOutlet NSColorWell *sectionLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *sectionDark;
+
+@property (nonatomic, weak) IBOutlet NSColorWell *outlineBackgroundLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *outlineBackgroundDark;
+@property (nonatomic, weak) IBOutlet NSColorWell *outlineHighlightLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *outlineHighlightDark;
+
+@property (nonatomic, weak) IBOutlet NSColorWell *outlineSectionLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *outlineSectionDark;
+
+@property (nonatomic, weak) IBOutlet NSColorWell *outlineSceneNumberLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *outlineSceneNumberDark;
+
+@property (nonatomic, weak) IBOutlet NSColorWell *outlineItemLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *outlineItemDark;
+
+@property (nonatomic, weak) IBOutlet NSColorWell *outlineItemOmittedLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *outlineItemOmittedDark;
+
+@property (nonatomic, weak) IBOutlet NSColorWell *outlineSynopsisLight;
+@property (nonatomic, weak) IBOutlet NSColorWell *outlineSynopsisDark;
+
+@property (nonatomic, weak) IBOutlet NSColorWell *genderWoman;
+@property (nonatomic, weak) IBOutlet NSColorWell *genderMan;
+@property (nonatomic, weak) IBOutlet NSColorWell *genderOther;
+@property (nonatomic, weak) IBOutlet NSColorWell *genderUnspecified;
+
 @property (nonatomic) NSTimer* updateTimer;
+
 @end
 
 @implementation ThemeEditor
@@ -31,6 +78,9 @@
 
 - (void)windowDidLoad {
     [super windowDidLoad];
+	
+	
+	
 	[self loadTheme:ThemeManager.sharedManager.theme];
 	
 }
@@ -53,7 +103,51 @@
 	[NSNotificationCenter.defaultCenter postNotification:[NSNotification notificationWithName:@"Reset theme" object:nil]];
 }
 - (void)loadTheme:(BeatTheme*)theme {
+	[_backgroundLight setColor:theme.backgroundColor.aquaColor];
+	[_backgroundDark setColor:theme.backgroundColor.darkAquaColor];
+	[_textLight setColor:theme.textColor.aquaColor];
+	[_textDark setColor:theme.textColor.darkAquaColor];
+	[_marginLight setColor:theme.marginColor.aquaColor];
+	[_marginDark setColor:theme.marginColor.darkAquaColor];
+	[_selectionLight setColor:theme.selectionColor.aquaColor];
+	[_selectionDark setColor:theme.selectionColor.darkAquaColor];
+	[_invisibleTextLight setColor:theme.invisibleTextColor.aquaColor];
+	[_invisibleTextDark setColor:theme.invisibleTextColor.darkAquaColor];
+	[_commentLight setColor:theme.commentColor.aquaColor];
+	[_commentDark setColor:theme.commentColor.darkAquaColor];
+	[_pageNumberLight setColor:theme.pageNumberColor.aquaColor];
+	[_pageNumberDark setColor:theme.pageNumberColor.darkAquaColor];
+	[_caretLight setColor:theme.caretColor.aquaColor];
+	[_caretDark setColor:theme.caretColor.darkAquaColor];
+	[_synopsisLight setColor:theme.synopsisTextColor.aquaColor];
+	[_synopsisDark setColor:theme.synopsisTextColor.darkAquaColor];
+	[_sectionLight setColor:theme.sectionTextColor.aquaColor];
+	[_sectionDark setColor:theme.sectionTextColor.darkAquaColor];
 	
+	[_outlineBackgroundLight setColor:theme.outlineBackground.aquaColor];
+	[_outlineBackgroundDark setColor:theme.outlineBackground.darkAquaColor];
+	[_outlineHighlightLight setColor:theme.outlineHighlight.aquaColor];
+	[_outlineHighlightDark setColor:theme.outlineHighlight.darkAquaColor];
+
+	[_outlineSceneNumberLight setColor:theme.outlineSceneNumber.aquaColor];
+	[_outlineSceneNumberDark setColor:theme.outlineSceneNumber.darkAquaColor];
+	
+	[_outlineItemLight setColor:theme.outlineItem.aquaColor];
+	[_outlineItemDark setColor:theme.outlineItem.darkAquaColor];
+	
+	[_outlineItemOmittedLight setColor:theme.outlineItemOmitted.aquaColor];
+	[_outlineItemOmittedDark setColor:theme.outlineItemOmitted.darkAquaColor];
+	
+	[_outlineSectionLight setColor:theme.outlineSection.aquaColor];
+	[_outlineSectionDark setColor:theme.outlineSection.darkAquaColor];
+	
+	[_outlineSynopsisLight setColor:theme.outlineSynopsis.aquaColor];
+	[_outlineSynopsisDark setColor:theme.outlineSynopsis.darkAquaColor];
+		
+	[_genderWoman setColor:theme.genderWomanColor.aquaColor];
+	[_genderMan setColor:theme.genderManColor.aquaColor];
+	[_genderOther setColor:theme.genderOtherColor.aquaColor];
+	[_genderUnspecified setColor:theme.genderUnspecifiedColor.aquaColor];
 }
 
 -(IBAction)changeColor:(id)sender {
