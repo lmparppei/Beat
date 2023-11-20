3e54030003b8c186f039cf1fb4a5f05c3de0d986
lmparppei <lauri-matti@kapitan.fi>
2023-11-20 23:14:37 +0200

    Revision management + localization

--- Beat/User Interface/Revision Management/BeatRevisionManagement.swift
+++ Beat/User Interface/Revision Management/BeatRevisionManagement.swift
@@ -62,4 +62,71 @@ import BeatCore.BeatRevisions
 		}
 	}
 	
+	@objc @IBAction func convertRevisions(_ sender:AnyObject?) {
+		guard let delegate = self.delegate else {
+			print("No editor delegate set")
+			return
+		}
+		
+		// Alert box
+		let input = NSAlert()
+		input.messageText = BeatLocalization.localizedString(forKey: "revisions.convert.title")
+		input.informativeText = BeatLocalization.localizedString(forKey: "revisions.convert.info")
+		
+		// Buttons
+		input.addButton(withTitle: BeatLocalization.localizedString(forKey: "general.ok"))
+		input.addButton(withTitle: BeatLocalization.localizedString(forKey: "general.cancel"))
+
+		// Accessory view
+		let accessoryView = NSView(frame: NSMakeRect(0, 0, 250, 125))
+		
+		let stack = NSStackView(frame: accessoryView.frame)
+		stack.orientation = .vertical
+		
+		let labelFrom = NSTextField(labelWithString: BeatLocalization.localizedString(forKey: "revisions.convert.from"))
+		let dropdownFrom = NSPopUpButton(frame: NSMakeRect(0.0, 0.0, 250.0, 30.0))
+		
+		let generations = BeatRevisions.revisionGenerations()
+						
+		let labelTo = NSTextField(labelWithString: BeatLocalization.localizedString(forKey: "revisions.convert.to"))
+		let dropdownTo = NSPopUpButton(frame: NSMakeRect(0.0, 0.0, 250.0, 30.0))
+		
+		stack.addArrangedSubview(labelFrom)
+		stack.addArrangedSubview(dropdownFrom)
+		stack.addArrangedSubview(labelTo)
+		stack.addArrangedSubview(dropdownTo)
+		
+		accessoryView.addSubview(stack)
+		input.accessoryView = accessoryView
+
+		// Add items
+		dropdownTo.addItem(withTitle: BeatLocalization.localizedString(forKey: "revisions.convert.remove"))
+		
+		for gen in generations {
+			let color = gen.color
+			
+			dropdownFrom.addItem(withTitle: BeatLocalization.localizedString(forKey: "color." + color))
+			dropdownFrom.itemArray.last?.image = NSImage(named: "Color_" + color.capitalized)
+			
+			dropdownTo.addItem(withTitle: BeatLocalization.localizedString(forKey: "color." + color))
+			dropdownTo.itemArray.last?.image = NSImage(named: "Color_" + color.capitalized)
+		}
+
+		input.beginSheetModal(for: delegate.documentWindow!) { response in
+			if response != .alertFirstButtonReturn {
+				return
+			}
+			
+			// Downgrade each revision
+			let originalGen = BeatRevisions.revisionGenerations()[dropdownFrom.indexOfSelectedItem]
+			
+			var targetGen:BeatRevisionGeneration?
+			if (dropdownTo.indexOfSelectedItem > 0) {
+				targetGen = BeatRevisions.revisionGenerations()[dropdownTo.indexOfSelectedItem - 1]
+			}
+			
+			self.convert(originalGen, to: targetGen)
+		}
+
+	}
 }
