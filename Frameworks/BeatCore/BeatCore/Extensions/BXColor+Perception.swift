b8444d72cf0fddffc33e7135dd90274d963ff16c
lmparppei <lauri-matti@kapitan.fi>
2025-03-07 11:44:02 +0200

    Cross-platform perceptive color

--- Frameworks/BeatCore/BeatCore/Extensions/BXColor+Perception.swift
+++ Frameworks/BeatCore/BeatCore/Extensions/BXColor+Perception.swift
@@ -14,11 +14,15 @@ import UIKit
 
 public extension BXColor {
     @objc var isDarkAsBackground: Bool {
-        if let ciColor = CIColor(color: self) { // Convert to CIColor for RGBA components
-            let luminance = (0.299 * ciColor.red) + (0.587 * ciColor.green) + (0.114 * ciColor.blue)
-            return luminance < 0.5
-        }
-        return true
-    }    
+        // macOS and iOS have different nullability for CIColor (wtf)
+        #if os(macOS)
+        guard let ciColor = CIColor(color: self) else { return false }
+        #else
+        let ciColor = CIColor(color: self)
+        #endif
+        
+        let luminance = (0.299 * ciColor.red) + (0.587 * ciColor.green) + (0.114 * ciColor.blue)
+        return luminance < 0.5
+    }
 }
 
